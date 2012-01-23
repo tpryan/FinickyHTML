e9ad9bff9689b3d521e55458a85bf8ec4f1bc7a6
Terrence Ryan <terry@terrenceryan.com>
2012-01-23 16:21:59 -0500

    Got the map working pretty well.

--- js/label.js
+++ js/label.js
@@ -1,54 +1,81 @@
 // Define the overlay, derived from google.maps.OverlayView
 function Label(opt_options) {
- // Initialization
- this.setValues(opt_options);
+  // Initialization
+  this.setValues(opt_options);
 
- // Label specific
- var span = this.span_ = document.createElement('span');
- span.style.cssText = 'position: relative; left: -50%; top: -48px; ' +
-                      'white-space: nowrap;  ' +
-                      'padding: 2px; z-index:100; font-family: LionsDenRegular';
 
- var div = this.div_ = document.createElement('div');
- div.appendChild(span);
- div.style.cssText = 'position: absolute; display: none';
+  // Label specific
+  var span = this.span_ = document.createElement('span');
+  span.style.cssText = ' display: block; position: relative; left: -50%; top: -58px; font-family: LionsDenRegular; '+
+  'overflow: hidden; text-overflow: ellipsis;' +
+  'padding: 2px; width: 90px; height: 30px;';
+
+
+  var div = this.div_ = document.createElement('div');
+  div.appendChild(span);
+  div.style.cssText = 'position: absolute;';
+  
 };
 Label.prototype = new google.maps.OverlayView;
 
+
 // Implement onAdd
 Label.prototype.onAdd = function() {
- var pane = this.getPanes().overlayLayer;
- pane.appendChild(this.div_);
+  var pane = this.getPanes().overlayImage;
+  pane.appendChild(this.div_);
 
- // Ensures the label is redrawn if the text or position is changed.
- var me = this;
- this.listeners_ = [
-   google.maps.event.addListener(this, 'position_changed',
-       function() { me.draw(); }),
-   google.maps.event.addListener(this, 'text_changed',
-       function() { me.draw(); })
- ];
+
+  // Ensures the label is redrawn if the text or position is changed.
+  var me = this;
+  this.listeners_ = [
+    google.maps.event.addListener(this, 'position_changed', function() { me.draw(); }),
+    google.maps.event.addListener(this, 'visible_changed', function() { me.draw(); }),
+    google.maps.event.addListener(this, 'clickable_changed', function() { me.draw(); }),
+    google.maps.event.addListener(this, 'text_changed', function() { me.draw(); }),
+    google.maps.event.addListener(this, 'zindex_changed', function() { me.draw(); }),
+    google.maps.event.addDomListener(this.div_, 'click', function() { 
+      if (me.get('clickable')) {
+        google.maps.event.trigger(me, 'click');
+      }
+    })
+  ];
 };
+
 
 // Implement onRemove
 Label.prototype.onRemove = function() {
- this.div_.parentNode.removeChild(this.div_);
+  this.div_.parentNode.removeChild(this.div_);
 
- // Label is removed from the map, stop updating its position/text.
- for (var i = 0, I = this.listeners_.length; i < I; ++i) {
-   google.maps.event.removeListener(this.listeners_[i]);
- }
+
+  // Label is removed from the map, stop updating its position/text.
+  for (var i = 0, I = this.listeners_.length; i < I; ++i) {
+    google.maps.event.removeListener(this.listeners_[i]);
+  }
 };
+
 
 // Implement draw
 Label.prototype.draw = function() {
- var projection = this.getProjection();
- var position = projection.fromLatLngToDivPixel(this.get('position'));
+  var projection = this.getProjection();
+  var position = projection.fromLatLngToDivPixel(this.get('position'));
 
- var div = this.div_;
- div.style.left = position.x + 'px';
- div.style.top = position.y + 'px';
- div.style.display = 'block';
 
- this.span_.innerHTML = this.get('text').toString();
+  var div = this.div_;
+  div.style.left = position.x + 'px';
+  div.style.top = position.y + 'px';
+
+
+  var visible = this.get('visible');
+  div.style.display = visible ? 'block' : 'none';
+
+
+  var clickable = this.get('clickable');
+  this.span_.style.cursor = clickable ? 'pointer' : '';
+
+
+  var zIndex = this.get('zIndex');
+  div.style.zIndex = zIndex;
+
+
+  this.span_.innerHTML = this.get('text').toString();
 };
